d477d0ad132a461df915cfba91ff366759810184
AnjulGupta <anjul.gupta@students.iiit.ac.in>
2018-09-01 21:28:03 +0530

    normal mode with curssor on up down key working

--- FE_project.cpp
+++ FE_project.cpp
@@ -1,19 +1,15 @@
 
 #include"normal_mode.h"
-#include<iostream>
-
-
-
 
 using namespace std;
 
 int main() {
 
   enter_normal_mode();
-  char ch;
-  while (read(0, &ch, 1) == 1 && ch != ':');
-  cout<<"\033[H\033[J";  
-  atexit(exit_normal_mode);
+ 
+  cout<<"\033[H\033[J";  //for clearing screen
+  
+  atexit(exit_normal_mode);  
   
   return 0;
 }
--- normal_mode.h
+++ normal_mode.h
@@ -1,14 +1,26 @@
 // normal mode
-#include "LS_cmd.h"
 #include <iostream>
-#include <unistd.h>         //STDIN_FILENO
-#include <termios.h>        // struct termios, tcgetattr(), tcsetattr(), TCSAFLUSH
-#include <stdlib.h>         //atexit(void func)
+#include <unistd.h>             //STDIN_FILENO
+#include <termios.h>            // struct termios, tcgetattr(), tcsetattr(), TCSAFLUSH
+#include <stdlib.h>             //atexit(void func)
+
+#include <iomanip>
+#include <stdio.h>
+#include <dirent.h>             // fdopendir(fd) , readdir(DIR *dp) 
+#include <fcntl.h>              //open(path,mode)
+#include <sys/stat.h>           //int stat(const char *restrict pathname, struct stat *restrict buf )
+#include <pwd.h>                //struct passwd, getpwuid() 
+#include <grp.h>                //struct grp, getgrgid()  
+#include <time.h>               //asctime(),localtime()
 
 
+
+void LS_cmd ();
 
 void enter_normal_mode();
 void exit_normal_mode();
 
+int keymove();
+int readch();
 
 
